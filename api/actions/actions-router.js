ccec513ebef3340c7caabb3f2e30fcd82e4b4d1f
jovan <jovancraft@gmail.com>
2024-01-23 00:51:40 -0600

    wip/still trying to figure out the problem with the first 2 endpoints

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,1 +1,26 @@
 // Write your "actions" router here!
+const express = require('express');
+const router = express.Router();
+const Actions = require('./actions-model');
+
+router.get('/api/actions', async (req, res) => {
+    // Implement the GET /api/actions endpoint
+  });
+
+  router.get('/api/actions/:id', async (req, res) => {
+    // Implement the GET /api/actions/:id endpoint
+  });
+
+  router.post('/api/actions', async (req, res) => {
+    // Implement the POST /api/actions endpoint
+  });
+
+  router.put('/api/actions/:id', async (req, res) => {
+    // Implement the PUT /api/actions/:id endpoint
+  });
+
+  router.delete('/api/actions/:id', async (req, res) => {
+    // Implement the DELETE /api/actions/:id endpoint
+  });
+
+  module.exports = router;
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,43 @@
 // Write your "projects" router here!
+const express = require('express')
+const Projects = require('./projects-model')
+const { validateProject } = require(`./projects-middleware`)
+const router = express.Router();
+
+router.get('/api/projects', (req, res) => {
+
+    Projects.get()
+    .then((projects) => {
+        if(projects){
+            res.status(200).json(projects)
+        } else {
+            res.status(200).json([])
+        }
+    })
+    .catch(() => {
+        res.status(500).json({ message: `An error has occurred while trying to bring up all the projects.`})
+    })
+
+  });
+
+  router.get('/api/projects/:id', (req, res, next) => {
+    const { id } = req.params
+    Projects.get(id)
+    .then((project) => {
+        if(project){
+            res.status(200).json(project)
+        } else {
+            res.status(404).json({ message: `There is no id by that number.`})
+        }
+    })
+    .catch((err) => {
+
+    })
+  })
+
+  router.post('/api/projects', (req, res) => {
+    
+  })
+
+
+module.exports = router
